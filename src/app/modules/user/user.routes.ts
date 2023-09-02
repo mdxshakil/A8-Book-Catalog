22bfcbe00c0aa0926dc06fcb481cf0a0a7a82662
mdxshakil <mdxshakil1767@gmail.com>
2023-09-02 12:50:12 +0600

    Added zod validation for user update

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
+import { UserValidation } from './user.validation';
 const router = express.Router();
 
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUser);
@@ -11,6 +13,7 @@ router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 router.patch(
   '/:id',
   auth(ENUM_USER_ROLE.ADMIN),
+  validateRequest(UserValidation.update),
   UserController.updateSingleUser
 );
 
