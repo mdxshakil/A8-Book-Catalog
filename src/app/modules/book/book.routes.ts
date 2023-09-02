9142e79d48a97aaf1e78bb9987d493c356c5d64e
mdxshakil <mdxshakil1767@gmail.com>
2023-09-02 13:54:04 +0600

    Added zod  validation for book module

--- src/app/modules/book/book.routes.ts
+++ src/app/modules/book/book.routes.ts
@@ -1,18 +1,35 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { BookController } from './book.controller';
+import { BookValidation } from './book.validation';
 const router = express.Router();
 
-router.get('/', BookController.getAllBooks);
-router.get('/:categoryId/category', BookController.getBooksByCategoryId);
-router.get('/:id', BookController.getSingleBook);
-router.patch('/:id', BookController.updateSingleBook);
-router.delete('/:id', BookController.deleteSingleBook);
 router.post(
   '/create-book',
+  validateRequest(BookValidation.create),
   auth(ENUM_USER_ROLE.ADMIN),
   BookController.createBook
+);
+
+router.get('/', BookController.getAllBooks);
+
+router.get('/:categoryId/category', BookController.getBooksByCategoryId);
+
+router.get('/:id', BookController.getSingleBook);
+
+router.patch(
+  '/:id',
+  validateRequest(BookValidation.update),
+  auth(ENUM_USER_ROLE.ADMIN),
+  BookController.updateSingleBook
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  BookController.deleteSingleBook
 );
 
 export const BookRoutes = router;
