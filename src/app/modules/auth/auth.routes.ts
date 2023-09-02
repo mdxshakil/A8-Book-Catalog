d9ea1f6431744ebccf9c206ab3c1cc2d861b5fd3
mdxshakil <mdxshakil1767@gmail.com>
2023-09-02 12:37:00 +0600

    Added zod validation for user signup

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AuthController } from './auth.controller';
+import { AuthValidation } from './auth.validation';
 const router = express.Router();
 
-router.post('/signup', AuthController.createUser);
+router.post(
+  '/signup',
+  validateRequest(AuthValidation.create),
+  AuthController.createUser
+);
 router.post('/signin', AuthController.loginUser);
 
 export const AuthRoutes = router;
---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+const create = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+    }),
+    email: z.string({
+      required_error: 'Email is required',
+    }),
+    password: z.string({
+      required_error: 'Password is required',
+    }),
+    role: z.enum(['customer', 'admin'] as [string, ...string[]], {
+      required_error: 'Role is required',
+    }),
+    contactNo: z.string({
+      required_error: 'Contact No. is required',
+    }),
+    address: z.string({
+      required_error: 'Address is required',
+    }),
+    profileImg: z.string({
+      required_error: 'profile Image is required',
+    }),
+  }),
+});
+
+export const AuthValidation = {
+  create,
+};
